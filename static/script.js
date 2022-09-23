350278b4809325de20838f904d22edca04f9f276
magnethilo <hello@thilo.fi>
2022-09-23 11:26:05 +0300

    Restructure tooltips into arc labels
    The dating reason for a particular relation is now displayed above the
    corresponding arc instead of overlaid as a text box.
    The new labels are <textPath> elements and part of the svg instead of
    regular DOM elements overlaid onto the svg. This fixes several problems,
    mainly panning and zooming concerns, which are now handled
    automatically by the textPath element.
    Some styling work remains.

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ let curDiagWidth = document
   .getBoundingClientRect()
   .width
 let curDiagHeight = curDiagWidth / GRAPH_ASPECT_RATIO + CIRCLE_RADIUS
-// Add a third of self for labels
+// Add a third of self for nodeLabels
 let labelAreaHeight = curDiagHeight / 3
 curDiagHeight = curDiagHeight + labelAreaHeight
 
@@ -18,7 +18,7 @@ const margin = {top: CIRCLE_RADIUS, right: CIRCLE_RADIUS, bottom: 0, left: CIRCL
 const width = curDiagWidth - margin.left - margin.right
 const height = curDiagHeight - margin.top - margin.bottom
 
-// Offset level of nodes by the space needed for labels
+// Offset level of nodes by the space needed for nodeLabels
 const GRAPH_BOTTOM_Y = height - labelAreaHeight
 
 // Debug
@@ -45,61 +45,46 @@ d3.json("/sound_changes").then(function(data) {
 
   // Arcs
   let arcs = svg
-    .selectAll("myarcs")
+    .selectAll("myArcs")
     .data(data.relations)
     .enter()
     .append("path")
+      .attr("id", (d, i) => "arc-" + i )
       .attr("d", function (relation) {
-      // X position of start node on the X axis
-      start = xScale(relation.source)
-      // X position of end node
-      end = xScale(relation.target)
-      // The arc starts at the coordinate 
-      // x=start, y=height-30 (where the starting node is)
-      // This means we're gonna build an elliptical arc
-      return ["M", start, GRAPH_BOTTOM_Y,
-        "A",
-        // Next 2 lines are the coordinates of the inflexion point.
-        // Height of this point is proportional with start - end distance
-        (start - end)/2, ",",
-        (start - end)/2, 0, 0, ",",
-        // We always want the arc on top. So if end is before start, 
-        // putting 0 here turn the arc upside down.
-        start < end ? 1 : 0, end, ",", GRAPH_BOTTOM_Y]
-        .join(' ');
+        // X position of start node on the X axis
+        start = xScale(relation.source)
+        // X position of end node
+        end = xScale(relation.target)
+        // The arc starts at the coordinate 
+        // x=start, y=height-30 (where the starting node is)
+        // This means we're gonna build an elliptical arc
+        return ["M", start, GRAPH_BOTTOM_Y,
+          "A",
+          // Next 2 lines are the coordinates of the inflexion point.
+          // Height of this point is proportional with start - end distance
+          (start - end)/2, ",",
+          (start - end)/2, 0, 0, ",",
+          // We always want the arc on top. So if end is before start, 
+          // putting 0 here turn the arc upside down.
+          start < end ? 1 : 0, end, ",", GRAPH_BOTTOM_Y]
+          .join(' ');
     })
 
-  // Get array of path coordinates (we need the top middle for tooltips)
-  // TODO: Change to D3 syntax for consistency
-  const arcElements = document.getElementsByTagName("path")
-  let arcApexes = []
-  for (const element of arcElements) {
-    posData = element.getBoundingClientRect()
-    const left = posData.left + posData.width / 2
-    const top = posData.top
-    arcApexes.push({"left": left, "top": top})
-  }
+  // Arc Labels
+  svg.append("text").attr("id", "arc-labels")
 
-  // Arc Tooltips
-  let arcTooltips = d3.select("#arc-tooltips")
-    .selectAll("mytooltips")
-    // Zip data with coordinates
-    .data(d3.zip(data.relations, arcApexes))
+  let arcLabels = d3.select("#arc-labels")
+    .selectAll("myArcLabels")
+    .data(data.relations)
     .enter()
-    .append("div")
-      .style("left", data => data[1].left + "px")
-      .style("top", data => {
-        // Labels are about 21px high currently
-        let lowestAllowedLabelHeight = GRAPH_BOTTOM_Y - CIRCLE_RADIUS - 22
-        let labelHeight = Math.min(data[1].top - 12, lowestAllowedLabelHeight)
-        return labelHeight + "px"
-      })
-      .html(data => data[0].d_reason)
-      .attr("class", "tooltip")
+    .append("textPath")
+      .attr("href", (d, i) => "#arc-" + i)
+      .attr("startOffset", "50%")
+      .html(relation => relation.d_reason)
 
   // Nodes
   let nodes = svg
-    .selectAll("mynodes")
+    .selectAll("myNodes")
     .data(data.changes)
     .enter()
     .append("circle")
@@ -108,9 +93,9 @@ d3.json("/sound_changes").then(function(data) {
       .attr("r", CIRCLE_RADIUS)
       .attr("class", "highlighted")
 
-  // Labels
-  let labels = svg
-      .selectAll("mylabels")
+  // Node Labels
+  let nodeLabels = svg
+      .selectAll("myNodeLabels")
       .data(data.changes)
       .enter()
       .append("text")
@@ -133,7 +118,7 @@ d3.json("/sound_changes").then(function(data) {
     }
   }
 
-  labels.each(truncate)
+  nodeLabels.each(truncate)
 
   let nodeTooltip = d3.select("#node-tooltip")
 
@@ -152,12 +137,12 @@ d3.json("/sound_changes").then(function(data) {
     // Get ids to be highlighted from arc data
     let highlight_ids = new Set(mArcsData.map(arc => [arc.source, arc.target]).flat())
     
-    // Pass to nodes and labels
+    // Pass to nodes and nodeLabels
     nodes
       .filter(node => highlight_ids.has(node.id))
       .classed("highlighted", true)
 
-    labels
+    nodeLabels
       // Removed (d, i) from input, not sure why it doesn't break stuff?
       .filter(i => highlight_ids.has(i + 1))
       .classed("highlighted", true)
@@ -177,8 +162,8 @@ d3.json("/sound_changes").then(function(data) {
       .html(m_node.name)
       .classed("highlighted", true)
 
-    arcTooltips
-      .filter(d => d[0].source === m_node.id || d[0].target === m_node.id)
+    arcLabels
+      .filter(rel => rel.source === m_node.id || rel.target === m_node.id)
       .classed("highlighted", true)
   })
   .on("mousemove", function(event, m_node){
@@ -189,56 +174,55 @@ d3.json("/sound_changes").then(function(data) {
   .on("mouseout", function(){
     nodes.classed("highlighted", true)
     arcs.classed("highlighted", false)
-    labels.classed("highlighted", false)
+    nodeLabels.classed("highlighted", false)
     nodeTooltip.classed("highlighted", false)
-    arcTooltips.classed("highlighted", false)
+    arcLabels.classed("highlighted", false)
   })
 
-  // Semantic zoom behavior
-  function handleZoom(zoomEvent) {
-    transform = zoomEvent.transform
-    let newScale = transform.rescaleX(xScale);
-
-    // Get new x positions from new scale
-    nodes.attr("cx", sc => newScale(sc.id))
-    labels.attr("x", sc => newScale(sc.id))
-
-    // Redraw arcs based on new scale
-    arcs
-      .attr("d", function (relation) {
-        // Get previous arc y radius, it stays constant
-        const d_string = d3.select(this).attr("d")
-        const ry = d_string.split(" ")[6]
-
-        start = newScale(relation.source)
-        end = newScale(relation.target)
-
-        // The commas are sacred, do not touch
-        return [
-          "M",                    // MoveTo 
-          start,                  // Starting x
-          GRAPH_BOTTOM_Y,         // Starting y
-          "A",                    // Elliptical Arc Curve
-          (start - end)/2, ",",   // rx
-          ry,                     // ry
-          0,                      // angle
-          0, ",",                 // large-arc-flag
-          start < end ? 1 : 0,    // sweep-flag
-          end, ",",               // Finishing x
-          GRAPH_BOTTOM_Y          // Finishing y
-        ]       
-        .join(' ');             
-      })
-    
-    // Hide node_tip when zooming
-    nodeTooltip.classed("highlighted", false)
-  }
-
-  // These lines are necessary for zoom to work
+  // Zoom behavior (semantic)
   let zoom = d3.zoom()
     .scaleExtent([1, 10])
     // Allow pan only inside diagram bounds
     .translateExtent([[0, 0], [curDiagWidth, curDiagHeight]])
-    .on('zoom', handleZoom);
+
+    // The meat of the zoom behavior
+    .on("zoom", zoomEvent => {
+      transform = zoomEvent.transform
+      let newScale = transform.rescaleX(xScale);
+
+      // Get new x positions from new scale
+      nodes.attr("cx", sc => newScale(sc.id))
+      nodeLabels.attr("x", sc => newScale(sc.id))
+
+      // Redraw arcs based on new scale
+      arcs
+        .attr("d", function (relation) {
+          // Get previous arc y radius, it stays constant
+          const d_string = d3.select(this).attr("d")
+          const ry = d_string.split(" ")[6]
+
+          start = newScale(relation.source)
+          end = newScale(relation.target)
+
+          // The commas are sacred, do not touch
+          return [
+            "M",                    // MoveTo 
+            start,                  // Starting x
+            GRAPH_BOTTOM_Y,         // Starting y
+            "A",                    // Elliptical Arc Curve
+            (start - end)/2, ",",   // rx
+            ry,                     // ry
+            0,                      // angle
+            0, ",",                 // large-arc-flag
+            start < end ? 1 : 0,    // sweep-flag
+            end, ",",               // Finishing x
+            GRAPH_BOTTOM_Y          // Finishing y
+          ]       
+          .join(' ');             
+        })
+      
+      // Hide node_tip when zooming
+      nodeTooltip.classed("highlighted", false)
+    })
   d3.select('svg').call(zoom);
 })
