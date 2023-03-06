0ec3cf377928fe3a7caa953394c0e4b35a255fdf
thilothelitz <hello@thilo.fi>
2023-03-06 16:34:18 +0200

    Draw ring for chord diagram

--- static/chord_diagram.js
+++ static/chord_diagram.js
@@ -1,6 +1,8 @@
 // Draw and manage interactive chord diagram, sidebar and examples
 // Based on https://d3-graph-gallery.com/graph/chord_basic.html
 
+const language = "Russian"
+
 const OUTER_WIDTH = document.getElementById("chord-diagram")
     .getBoundingClientRect().width
 const OUTER_HEIGHT = document.getElementById("chord-diagram")
@@ -9,18 +11,26 @@ const OUTER_HEIGHT = document.getElementById("chord-diagram")
 console.log("Width:" + OUTER_WIDTH)
 console.log("Height:" + OUTER_HEIGHT)
 
-const MARGIN = {TOP: CIRCLE_RADIUS, RIGHT: CIRCLE_RADIUS + 10, BOTTOM: 0,
-  LEFT: CIRCLE_RADIUS + 10}
-const INNER_WIDTH = OUTER_WIDTH - MARGIN.LEFT - MARGIN.RIGHT
-const INNER_HEIGHT = OUTER_HEIGHT - MARGIN.TOP - MARGIN.BOTTOM
-const GRAPH_BOTTOM_Y = INNER_HEIGHT - LABEL_AREA_HEIGHT
+// Arbitrary division
+const RADIUS = OUTER_WIDTH / 5
+// const MARGIN = OUTER_WIDTH / 2
+const MARGIN = {
+  TOP: OUTER_HEIGHT / 2, 
+  RIGHT: OUTER_WIDTH / 2, 
+  BOTTOM: OUTER_HEIGHT / 2,
+  LEFT: OUTER_WIDTH / 2
+}
+
+console.log("Radius:" + RADIUS)
+console.log("Margin:" + MARGIN)
+
 
 let offcanvasDrawerEl = document.getElementById("offcanvasRight")
 let offcanvasDrawerObj = new bootstrap.Offcanvas(offcanvasDrawerEl)
 let lockOriginNodeId = null
 
 // SVG AND GROUPING ELEMENT SETUP
-const svg = d3.select("#arc-diagram")
+const svg = d3.select("#chord-diagram")
   .append("svg")
   .attr("xmlns", "http://www.w3.org/2000/svg")
   .attr("xlink", "http://www.w3.org/1999/xlink")
@@ -28,4 +38,31 @@ const svg = d3.select("#arc-diagram")
   .attr("height", OUTER_HEIGHT)
 
 const diagram = svg.append("g")
-  .attr("transform", "translate(" + MARGIN.LEFT + "," + MARGIN.TOP + ")")
+  .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`)
+
+Promise.all([
+  d3.json(`/dependency_wheel_old?lang=${language}`),
+  d3.json(`/examples?lang=${language}`),
+]).then(function([sc_data, example_data]) {
+  // Set up chord layout
+  chord = d3.chord()
+    .padAngle(0.02)
+
+  // Actually load data
+  chords = chord(sc_data.matrix)
+  
+  diagram
+    .datum(chords)
+    .selectAll("g")
+    .data(function(d) { return d.groups; })
+    .enter()
+    .append("g")
+    .append("path")
+      // .style("fill", "grey")
+      // .style("stroke", "black")
+      .attr("class", "ring-el")
+      .attr("d", d3.arc()
+        .innerRadius(RADIUS)
+        .outerRadius(RADIUS + 10)
+      )
+})
